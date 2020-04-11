f770768a3dbf089163af77a4e57c837e5c5c0cea
nielsbohr <root@DESKTOP-3QCMO93.localdomain>
2020-04-11 23:59:06 +0300

    - carousel

--- js/main.js
+++ js/main.js
@@ -31,13 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 '</div>'
 });
 
-$(document).ready(function(){
-
-    var carousel = $("#carousel");
-
-    carousel.owlCarousel();
-});
-
 document.addEventListener("DOMContentLoaded", function () {
     const buttonsContainer = document.querySelector(".buttons-wrap");
     const fridgeInfoContainer = document.querySelector(".card_size_m:nth-child(8) .card-description");
